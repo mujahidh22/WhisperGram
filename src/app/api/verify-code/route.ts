4bfee29166a59b3d3a34fabc65e13f2a359544e5
mujahidh22 <mujahid442@gmail.com>
2025-08-31 21:15:37 +0530

    signIn page UI

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -35,8 +35,7 @@ export async function POST(request: Request) {
             return Response.json(
                 {
                     success: false,
-                    message:
-                        'Verification code has expired. Please sign up again to get a new code.',
+                    message: 'Verification code has expired. Please sign up again to get a new code.',
                 },
                 { status: 400 }
             );
